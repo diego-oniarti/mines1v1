6aded4b517bdb7978c21c390bca179f82d6d1e47
Diego <69463084+diego-oniarti@users.noreply.github.com>
2024-10-20 23:06:51 +0200

    esempio websocket

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module hello
+module github.com/diego-oniarti/mines1v1
 
 go 1.23.2
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -4,15 +4,44 @@ import (
     "fmt"
     "log"
     "net/http"
+    "github.com/gorilla/websocket"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+var upgrader = websocket.Upgrader {
+    CheckOrigin: func(r *http.Request) bool {return true;},
+}
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w,r,nil);
+    if err != nil {
+        log.Println("Error upgrading to websocker:", err);
+        return;
+    }
+    defer conn.Close();
+
+    for {
+        messageType, message, err := conn.ReadMessage()
+        if err != nil {
+            log.Println("Error reading message:", err);
+            break;
+        }
+        log.Printf("Received: %s", message);
+        if err := conn.WriteMessage(messageType, message); err != nil {
+            log.Println("Error writing message:", err);
+            break;
+        }
+    }
+}
+
 func main() {
     // http.HandleFunc("/", handler)
     file_server := http.StripPrefix("/static/", http.FileServer(http.Dir("./static")));
     http.Handle("/static/", file_server);
+
+    http.HandleFunc("/ws", websocketHandler);
+
     log.Fatal(http.ListenAndServe(":8080", nil));
 }
