ea6d2e0724e8c0de12a62e17dd52e94e611b444f
Diego <69463084+diego-oniarti@users.noreply.github.com>
2024-11-17 14:49:48 +0100

    fix login

--- shared/sessions.go
+++ shared/sessions.go
@@ -1,14 +1,20 @@
 package shared
 
 import (
+	"log"
 	"os"
 
 	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
 )
 
-var Store *sessions.CookieStore = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
+var Store *sessions.CookieStore 
 
 func init() {
+    err := godotenv.Load(".env")
+    if err != nil { panic("Error loading .env file") }
+    Store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
+    log.Println(os.Getenv("SESSION_SECRET"))
     Store.Options = &sessions.Options{
     	Path:        "/",
     	Domain:      "localhost",
