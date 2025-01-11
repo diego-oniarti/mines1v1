da2afc7d6c37cd98044c115a3ff5443ea5554cbe
Diego <69463084+diego-oniarti@users.noreply.github.com>
2025-01-11 22:57:35 +0100

    aggiunte le animazioni per l'1v1 e la fine partita. Manca ancora il rigioca

--- gamemodes/1v1.go
+++ gamemodes/1v1.go
@@ -1,160 +1,176 @@
 package gamemodes
 
 import (
-    "log"
-    "net/http"
-    "time"
+	"log"
+	"net/http"
+	"time"
 
-    "github.com/diego-oniarti/mines1v1/shared"
-    "github.com/gin-gonic/gin"
-    _ "github.com/gorilla/websocket"
+	"github.com/diego-oniarti/mines1v1/shared"
+	"github.com/gin-gonic/gin"
+	_ "github.com/gorilla/websocket"
 )
 
 func M1v1Ws(c *gin.Context) {
-    conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-    if err != nil {
-        log.Println("Cannot create websocket");
-        c.String(http.StatusInternalServerError, "Impossibile creare WebSocket")
-        return
-    }
-    defer conn.Close()
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("Cannot create websocket")
+		c.String(http.StatusInternalServerError, "Impossibile creare WebSocket")
+		return
+	}
+	defer conn.Close()
 
-    // Il client manda un messaggio con il game_id (stringa)
-    messageType, game_id, err := conn.ReadMessage()
-    if err != nil || messageType!=1 {
-        log.Println(err)
-        return 
-    }
+	// Il client manda un messaggio con il game_id (stringa)
+	messageType, game_id, err := conn.ReadMessage()
+	if err != nil || messageType != 1 {
+		log.Println(err)
+		return
+	}
 
-    game_id_str := string(game_id[:])
-    game_instance, ok := games[game_id_str]
-    if !ok { return } // se il game_id non esiste esci
-    game_params := game_instance.params
+	game_id_str := string(game_id[:])
+	game_instance, ok := games[game_id_str]
+	if !ok {
+		return
+	} // se il game_id non esiste esci
+	game_params := game_instance.params
 
-    // Manda i parametri al client
-    err = conn.WriteMessage(2, arrToBuff([]uint16{
-        game_params.width,
-        game_params.height,
-        game_params.bombs,
-        game_params.tempo,
-    }))
+	// Manda i parametri al client
+	is_g1 := game_instance.g1 == nil
 
-    is_g1 := true
-    to_other_chn := game_instance.a_to_b
-    from_other_chn := game_instance.b_to_a
-    other_conn := game_instance.g2 // Qui è nil
-    if game_instance.g1==nil {
-        game_instance.g1 = conn
-        defer delete(games, game_id_str) // Lascia chiudere solo il primo. Così se il secondo non arriva mai chiude comunque
+	var player_number uint16 = 0
+	if !is_g1 {
+		player_number = 1
+	}
+	err = conn.WriteMessage(2, arrToBuff([]uint16{
+		game_params.width,
+		game_params.height,
+		game_params.bombs,
+		game_params.tempo,
+		player_number,
+	}))
 
-        <-from_other_chn // Aspetta si sia connesso G2
-        other_conn = game_instance.g2
-    }else{
-        game_instance.g2 = conn
-        is_g1 = false
-        to_other_chn = game_instance.b_to_a
-        from_other_chn = game_instance.a_to_b
-        other_conn = game_instance.g1
+	to_other_chn := game_instance.a_to_b
+	from_other_chn := game_instance.b_to_a
+	other_conn := game_instance.g2 // Qui è nil
+	if is_g1 {
+		game_instance.g1 = conn
+		defer delete(games, game_id_str) // Lascia chiudere solo il primo. Così se il secondo non arriva mai chiude comunque
 
-        to_other_chn <- 1 // avvisa che si è connesso G2
-    }
+		<-from_other_chn // Aspetta si sia connesso G2
+		other_conn = game_instance.g2
+	} else {
+		game_instance.g2 = conn
+		to_other_chn = game_instance.b_to_a
+		from_other_chn = game_instance.a_to_b
+		other_conn = game_instance.g1
 
-    var game *Game
-    var timer <-chan time.Time
-    isFirstMove := is_g1; // g2 non ha mai la prima mossa
-    waiting_other := false // G2 inizia aspettando l'altro
+		to_other_chn <- 1 // avvisa che si è connesso G2
+	}
 
-    if !is_g1 { // Aspetta la prima mossa dal g1
-        <- from_other_chn
-        game = game_instance.game // Settato da g1
-    }
+	var game *Game
+	var timer <-chan time.Time
+	isFirstMove := is_g1   // g2 non ha mai la prima mossa
+	waiting_other := false // G2 inizia aspettando l'altro
 
-    move_chn := make(chan []byte, 1)
-    message_type_chn := make(chan int, 1)
-    error_chn := make(chan error, 1)
+	if !is_g1 { // Aspetta la prima mossa dal g1
+		<-from_other_chn
+		game = game_instance.game // Settato da g1
+	}
 
-    go func() {
-        for {
-            messageType, move, err := conn.ReadMessage()
-            if err!=nil {
-                error_chn <- err
-                return
-            }
-            message_type_chn <- messageType
-            move_chn <- move
-        }
-    }()
-    
-    for {
-        var move []byte
-        var messageType int
-        var err error
-        select {
-        case move=<-move_chn:
-            messageType=<-message_type_chn
-            if waiting_other {
-                continue
-            } // Scarta i messaggi mandati mentre in attesa
-            if messageType != 1 && messageType != 2 {
-                return
-            }
-            x,y,flag := bytesToMove(move)
+	move_chn := make(chan []byte, 1)
+	message_type_chn := make(chan int, 1)
+	error_chn := make(chan error, 1)
 
-            if isFirstMove {
-                if flag { continue } // Ignora la prima mossa se è una flag
+	go func() {
+		for {
+			messageType, move, err := conn.ReadMessage()
+			if err != nil {
+				error_chn <- err
+				return
+			}
+			message_type_chn <- messageType
+			move_chn <- move
+		}
+	}()
 
-                // G1 crea il game
-                if is_g1 {
-                    game = NewGame(game_params.width, game_params.height,
-                    game_params.bombs, game_params.tempo,
-                    x,y)
-                    game_instance.game = game
-                    timer = get_timer(&game_params)
-                }
-                isFirstMove = false
-            }
+	for {
+		var move []byte
+		var messageType int
+		var err error
+		select {
+		case move = <-move_chn:
+			messageType = <-message_type_chn
+			if waiting_other {
+				continue
+			} // Scarta i messaggi mandati mentre in attesa
+			if messageType != 1 && messageType != 2 {
+				return
+			}
+			x, y, flag := bytesToMove(move)
 
-            if flag {
-                flagged, err := game.flag(x, y)
-                if err!=nil {
-                    log.Println(err)
-                    continue
-                }else{
-                    send_flagged(flagged, x, y, conn, false)
-                    send_flagged(flagged, x, y, other_conn, true)
-                }
-            }else{
-                changes, err := game.click(x,y)
-                if err!=nil {
-                    log.Println(err)
-                    continue
-                }
-                send_changes(&changes, conn, game.state, false)
-                send_changes(&changes, other_conn, game.state, true)
-                to_other_chn <- 1
-                waiting_other = true                 // Dopo aver fatto la mossa inizia ad aspettare l'altro
-            }
-        case err=<-error_chn:
-            log.Println(err)
-            return
-        case <-timer:
-            if waiting_other { continue } // Scarta i messaggi mandati mentre in attesa
-        case <- from_other_chn:
-            waiting_other = false
-        }
-    }
+			if isFirstMove {
+				if flag {
+					continue
+				} // Ignora la prima mossa se è una flag
+
+				// G1 crea il game
+				if is_g1 {
+					game = NewGame(game_params.width, game_params.height,
+						game_params.bombs, game_params.tempo,
+						x, y)
+					game_instance.game = game
+					timer = get_timer(&game_params)
+				}
+				isFirstMove = false
+			}
+
+			if flag {
+				flagged, err := game.flag(x, y)
+				if err != nil {
+					log.Println(err)
+					continue
+				} else {
+					send_flagged(flagged, x, y, conn, false)
+					send_flagged(flagged, x, y, other_conn, true)
+				}
+			} else {
+				changes, err := game.click(x, y)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				send_changes(&changes, conn, game.state, false)
+				send_changes(&changes, other_conn, game.state, true)
+				to_other_chn <- 1
+				waiting_other = true // Dopo aver fatto la mossa inizia ad aspettare l'altro
+			}
+		case err = <-error_chn:
+			log.Println(err)
+			return
+		case <-timer:
+			if waiting_other {
+				continue
+			} // Scarta i messaggi mandati mentre in attesa
+			changes := game.get_loosing_message()
+			game.state = Lost
+			send_changes(&changes, conn, game.state, false)
+			send_changes(&changes, other_conn, game.state, true)
+		case <-from_other_chn:
+			waiting_other = false
+			timer = get_timer(&game_params)
+		}
+	}
 }
 
 func M1v1Page(c *gin.Context) {
-    game_id := c.Query("game_id")
-    if game_id=="" {
-        c.Status(400)
-        return
-    }
-    // controlla che il game_id sia valido. Previene reflect injection
-    if _, present := games[game_id]; !present {
-        c.Status(400)
-        return
-    }
-    shared.Render(c, http.StatusOK, "1v1.html", gin.H{"game_id": game_id});
+	game_id := c.Query("game_id")
+	if game_id == "" {
+		c.Status(400)
+		return
+	}
+	// controlla che il game_id sia valido. Previene reflect injection
+	if _, present := games[game_id]; !present {
+		c.Status(400)
+		return
+	}
+	shared.Render(c, http.StatusOK, "1v1.html", gin.H{"game_id": game_id})
 }
--- static/scripts/M1v1.js
+++ static/scripts/M1v1.js
@@ -7,141 +7,190 @@ const endgame_controls = document.querySelector("#endgame_controls");
 const play_again = document.querySelector("#replay");
 const goback = document.querySelector("#goback");
 
-socket.addEventListener("open", ()=>{
+socket.addEventListener("open", () => {
     socket.send(game_id);
 });
 
-let grid_width,grid_height,tot_bombs,time;
-let a; let b = new Promise(r=>{a=r});
-let cellSize=40;
+let grid_width, grid_height, tot_bombs, time;
+let turn = false;
+
+let a;
+let b = new Promise((r) => {
+    a = r;
+});
+let cellSize = 40;
 
 class Cella {
     constructor(flag, number, bomb, player) {
         this.flag = flag;
         this.number = number;
         this.bomb = bomb;
-        this.player = player
+        this.player = player;
     }
 }
 /** @type{Cella[][]} */
-    const celle = [];
+const celle = [];
+
+let circles = [];
 
 async function setup() {
     await b;
-    let W = grid_width*cellSize;
-    let H = grid_height*cellSize;
+    let W = grid_width * cellSize;
+    let H = grid_height * cellSize;
     if (W > section.clientWidth) {
-        cellSize = section.clientWidth/grid_width;
-        W = grid_width*cellSize;
-        H = grid_height*cellSize;
+        cellSize = section.clientWidth / grid_width;
+        W = grid_width * cellSize;
+        H = grid_height * cellSize;
     }
-    if (H>window.innerHeight*0.9) {
-        cellSize = window.innerHeight*0.9/grid_height;
-        W = grid_width*cellSize;
-        H = grid_height*cellSize;
+    if (H > window.innerHeight * 0.9) {
+        cellSize = (window.innerHeight * 0.9) / grid_height;
+        W = grid_width * cellSize;
+        H = grid_height * cellSize;
     }
 
     const canvas = createCanvas(W, H);
     canvas.parent(box);
-    section.addEventListener('contextmenu', event => event.preventDefault());
+    section.addEventListener("contextmenu", (event) => event.preventDefault());
     resizeCollapsable();
 }
 
 let timer_start;
 function draw() {
-    if (phase==phases.GetGameParams) return;
-    background(200);
-    stroke(150);
-    strokeWeight(1);
-    for (let i=1; i<grid_width; i++) {
-        line(i*cellSize,0, i*cellSize,height);
+    if (phase == phases.GetGameParams) return;
+    background(240);
+    stroke(0);
+    strokeWeight(0.1);
+    for (let i = 1; i < grid_width; i++) {
+        line(i * cellSize, 0, i * cellSize, height);
     }
-    for (let i=1; i<grid_height; i++) {
-        line(0,i*cellSize, width, i*cellSize);
+    for (let i = 1; i < grid_height; i++) {
+        line(0, i * cellSize, width, i * cellSize);
     }
 
     textAlign(CENTER, CENTER);
-    textSize(cellSize*0.8);
+    textSize(cellSize * 0.8);
     noStroke();
-    for (let y=0; y<grid_height; y++) {
-        for (let x=0; x<grid_width; x++) {
+    for (let y = 0; y < grid_height; y++) {
+        for (let x = 0; x < grid_width; x++) {
             if (!celle[y][x]) continue;
             const cella = celle[y][x];
             noStroke();
             if (cella.flag) {
                 if (cella.player) {
                     fill(0, 255, 255);
-                }else{
+                } else {
                     fill(255, 0, 255);
                 }
-                text("🏳", (x+0.5)*cellSize, (y+0.5)*cellSize);
+                text("🏳", (x + 0.5) * cellSize, (y + 0.5) * cellSize);
                 continue;
             }
-            fill(150);
-            rect(x*cellSize, y*cellSize, cellSize, cellSize);
+            fill(220);
+            rect(x * cellSize, y * cellSize, cellSize, cellSize);
             if (cella.player) {
-                fill(0, 255, 255);
-            }else{
-                fill(255, 0, 255);
+                fill(0, 190, 190);
+            } else {
+                fill(190, 0, 190);
             }
             if (cella.bomb) {
-                stroke(200,0,0);
-                line(x*cellSize, y*cellSize,(x+1)*cellSize, (y+1)*cellSize);
-                line((x+1)*cellSize, y*cellSize,x*cellSize, (y+1)*cellSize);
+                stroke(200, 0, 0);
+                line(
+                    x * cellSize,
+                    y * cellSize,
+                    (x + 1) * cellSize,
+                    (y + 1) * cellSize,
+                );
+                line(
+                    (x + 1) * cellSize,
+                    y * cellSize,
+                    x * cellSize,
+                    (y + 1) * cellSize,
+                );
                 noStroke();
                 continue;
             }
-            if (cella.number!=0) {
-                text(cella.number, (x+0.5)*cellSize, (y+0.5)*cellSize);
+            if (cella.number != 0) {
+                text(cella.number, (x + 0.5) * cellSize, (y + 0.5) * cellSize);
             }
         }
     }
 
+    const now = new Date();
+    const circle_max = 200;
+    circles = circles.filter((c) => {
+        return now - c.start < circle_max;
+    });
+    noFill();
+    strokeWeight(4);
+    for (let c of circles) {
+        stroke(190, 40, 40, map(now - c.start, 0, circle_max, 255, 0));
+        circle(
+            (c.x + 0.5) * cellSize,
+            (c.y + 0.5) * cellSize,
+            map(now - c.start, 0, circle_max, 0, cellSize),
+        );
+    }
+
     switch (phase) {
         case phases.GetUpdates:
-            if (time==0 || !timer_start) break;
-            const R = Math.min(width,height)*0.8
-            const c = color(180,40,40,30);
+            if (time == 0 || !timer_start) break;
+            const R = Math.min(width, height) * 0.8;
+            const c = turn ? color(180, 40, 40, 30) : color(40, 40, 180, 30);
             stroke(c);
             noFill();
             strokeWeight(20);
-            circle(width/2,height/2, R);
+            circle(width / 2, height / 2, R);
             noStroke();
             fill(c);
-            arc(width/2, height/2, R-20, R-20, map(new Date()-timer_start, 0, time, 0,TWO_PI)-PI/2, -PI/2);
+            arc(
+                width / 2,
+                height / 2,
+                R - 20,
+                R - 20,
+                map(new Date() - timer_start, 0, time, 0, TWO_PI) - PI / 2,
+                -PI / 2,
+            );
             break;
         case phases.Won:
-            textSize(height/5);
+            textSize(height / 5);
             textStyle(BOLD);
-            fill(50,200,50);
-            stroke(50,100,50);
+            fill(50, 200, 50);
+            stroke(50, 100, 50);
             strokeWeight(2);
-            text("YOU WON", width/2, height/2);
+            text("YOU WON", width / 2, height / 2);
+            textStyle(NORMAL);
+            break;
+        case phases.Draw:
+            textSize(height / 5);
+            textStyle(BOLD);
+            fill(0, 0, 200);
+            stroke(100, 50, 50);
+            strokeWeight(2);
+            text("DRAW", width / 2, height / 2);
             textStyle(NORMAL);
             break;
         case phases.Lost:
-            textSize(height/5);
+            textSize(height / 5);
             textStyle(BOLD);
-            fill(200,0,0);
-            stroke(100,50,50);
+            fill(200, 0, 0);
+            stroke(100, 50, 50);
             strokeWeight(2);
-            text("YOU LOST", width/2, height/2);
+            text("YOU LOST", width / 2, height / 2);
             textStyle(NORMAL);
             break;
     }
 }
-
 function mousePressed() {
-    if (mouseX>width||mouseX<0||mouseY<0||mouseY>height) return true;
+    if (mouseX > width || mouseX < 0 || mouseY < 0 || mouseY > height)
+        return true;
     if (phase != phases.GetUpdates) return true;
-    const x = Math.floor(mouseX/cellSize);
-    const y = Math.floor(mouseY/cellSize);
-    const flag = mouseButton!=LEFT;
+    const x = Math.floor(mouseX / cellSize);
+    const y = Math.floor(mouseY / cellSize);
+    const flag = mouseButton != LEFT;
     const bits = new ArrayBuffer(5);
     const view = new DataView(bits);
     view.setInt16(0, x);
     view.setInt16(2, y);
-    view.setInt8(4, flag?1:0);
+    view.setInt8(4, flag ? 1 : 0);
 
     socket.send(bits);
 
@@ -153,11 +202,12 @@ const phases = {
     GetUpdates: 2,
     Won: 3,
     Lost: 4,
-}
+    Draw: 5,
+};
 let phase = phases.GetGameParams;
 
-socket.addEventListener("message", e=>{
-    e.data.arrayBuffer().then(ab=>{
+socket.addEventListener("message", (e) => {
+    e.data.arrayBuffer().then((ab) => {
         const data_view = new DataView(ab);
 
         switch (phase) {
@@ -173,80 +223,113 @@ socket.addEventListener("message", e=>{
 
 function get_game_params(data_view) {
     const data = [];
-    for (let i=0; i<4; i++) {
-        data.push(data_view.getUint16(i*2))
+    for (let i = 0; i < 5; i++) {
+        data.push(data_view.getUint16(i * 2));
     }
-    [grid_width,grid_height,tot_bombs,time] = data;
+    let player_num;
+    [grid_width, grid_height, tot_bombs, time, player_num] = data;
+    if (player_num == 0) {
+        turn = true;
+    }
     a();
     phase = phases.GetUpdates;
-    for (let y=0; y<grid_width; y++) {
-        celle.push([])
-        for (let x=0; x<grid_width; x++) {
-            celle[y].push(null)
+    for (let y = 0; y < grid_width; y++) {
+        celle.push([]);
+        for (let x = 0; x < grid_width; x++) {
+            celle[y].push(null);
         }
     }
 }
 
 /**
-    * @param {DataView} data_view 
-    */
+ * @param {DataView} data_view
+ */
 
-    function get_updates(data_view) {
-        const first_byte = data_view.getInt8(0);
-        const type = first_byte >> 6;
-        const player = (first_byte & 1) == 0
-        switch (type) {
-            case 0:
-                const gameover = (first_byte & 0b00100000)>0;
-                const won = (first_byte & 0b00010000)>0;
-                const lost = gameover && !won;
-                let off = 1;
-                let has_next = true;
-                do {
-                    const x = data_view.getUint16(off); off+=2;
-                    const y = data_view.getUint16(off); off+=2;
-                    const payload = data_view.getUint8(off); off+=1;
-                    const num = payload>>4;
-                    has_next = (payload&0b00001000)>0;
+function get_updates(data_view) {
+    const first_byte = data_view.getInt8(0);
+    const type = first_byte >> 6;
+    const player = (first_byte & 1) == 0;
+    let first_x, first_y;
+    switch (type) {
+        case 0:
+            const gameover = (first_byte & 0b00100000) > 0;
+            const won = (first_byte & 0b00010000) > 0;
+            const lost = gameover && !won;
+            let off = 1;
+            let has_next = true;
 
-                    if (lost) {
-                        celle[y][x] = new Cella(false, 0, true, player);
-                    }else{
-                        celle[y][x] = new Cella(false, num, false, player);
-                    }
-                } while (has_next);
+            let is_first = true;
 
-                if (gameover) {
-                    phase = won ? phases.Won : phases.Lost;
-                    console.log("timer null");
-                    timer_start=null
-                    show_endgame_controls();
-                    return;
+            do {
+                const x = data_view.getUint16(off);
+                off += 2;
+                const y = data_view.getUint16(off);
+                off += 2;
+                const payload = data_view.getUint8(off);
+                off += 1;
+                const num = payload >> 4;
+                has_next = (payload & 0b00001000) > 0;
+
+                if (lost) {
+                    celle[y][x] = new Cella(false, 0, true, player);
                 } else {
-                    timer_start = new Date();
+                    celle[y][x] = new Cella(false, num, false, player);
                 }
-                break;
-            case 1:
-                const flag = (first_byte&0b00100000)>0;
-                const x = data_view.getUint16(1);
-                const y = data_view.getUint16(3);
-                if (flag) {
-                    celle[y][x] = new Cella(true, 0, false, player);
-                }else{
-                    celle[y][x] = null;
+
+                if (is_first) {
+                    first_x = x;
+                    first_y = y;
+                    is_first = false;
                 }
-                break;
-        }
+            } while (has_next);
+
+            if (gameover) {
+                if (won) {
+                    phase = phases.Draw;
+                } else {
+                    if (turn) {
+                        phase = phases.Lost;
+                    } else {
+                        phase = phases.Won;
+                    }
+                }
+                console.log("timer null");
+                timer_start = null;
+                show_endgame_controls();
+                return;
+            } else {
+                timer_start = new Date();
+            }
+
+            turn = !turn;
+            break;
+        case 1:
+            const flag = (first_byte & 0b00100000) > 0;
+            const x = data_view.getUint16(1);
+            const y = data_view.getUint16(3);
+            first_x = x;
+            first_y = y;
+            if (flag) {
+                celle[y][x] = new Cella(true, 0, false, player);
+            } else {
+                celle[y][x] = null;
+            }
+            break;
     }
+
+    if (!player) {
+        circles.push({ x: first_x, y: first_y, start: new Date() });
+    }
+}
 
 function show_endgame_controls() {
     endgame_controls.classList.add("form_fields");
     resizeCollapsable();
 }
 
-play_again.addEventListener("click", ()=>{
+play_again.addEventListener("click", () => {
     for (let row of celle) {
-        for (let i=0; i<row.length; i++) {
+        for (let i = 0; i < row.length; i++) {
             row[i] = null;
         }
     }
