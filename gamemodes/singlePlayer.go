c62915e774c9d34b23e4a889fa635f803bfc0134
Diego <69463084+diego-oniarti@users.noreply.github.com>
2025-01-12 15:00:08 +0100

    iniziato a lavorare al timer. Al lato server viene stampato un tempo quando si preme PlayAgain per qualche motivo. Il tempo ufficiale va mandato dal server, quindi vanno aggiunti nuovi messaggi

--- gamemodes/singlePlayer.go
+++ gamemodes/singlePlayer.go
@@ -11,141 +11,150 @@ import (
 )
 
 func SinglePlayerWs(c *gin.Context) {
-    conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-    if err != nil {
-        log.Println("Cannot create websocket");
-        c.String(http.StatusInternalServerError, "Impossibile creare WebSocket")
-        return
-    }
-    defer conn.Close()
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("Cannot create websocket")
+		c.String(http.StatusInternalServerError, "Impossibile creare WebSocket")
+		return
+	}
+	defer conn.Close()
 
-    messageType, game_id, err := conn.ReadMessage()
-    if err != nil || messageType!=1 { return }
+	messageType, game_id, err := conn.ReadMessage()
+	if err != nil || messageType != 1 {
+		return
+	}
 
-    game_id_str := string(game_id[:])
-    game_instance, ok := games[game_id_str]
-    if !ok { return }
-    game_params := game_instance.params
-    defer delete(games, game_id_str)
+	game_id_str := string(game_id[:])
+	game_instance, ok := games[game_id_str]
+	if !ok {
+		return
+	}
+	game_params := game_instance.params
+	defer delete(games, game_id_str)
 
-    err = conn.WriteMessage(2, arrToBuff([]uint16{
-        game_params.width,
-        game_params.height,
-        game_params.bombs,
-        game_params.tempo,
-    }))
+	err = conn.WriteMessage(2, arrToBuff([]uint16{
+		game_params.width,
+		game_params.height,
+		game_params.bombs,
+		game_params.tempo,
+	}))
 
-    var game *Game
-    var timer <-chan time.Time
+	var game *Game
+	var timer <-chan time.Time
 
-    // loop esterno. Ogni ciclo è una partita
-    for {
-        isFirstMove := true;
+	// loop esterno. Ogni ciclo è una partita
+	for {
+		isFirstMove := true
+		var startTime time.Time
+		// loop interno. Ogni ciclo è una mossa nella partita
+		for game == nil || game.state == Running {
+			move_chn := make(chan []byte, 1)
+			message_type_chn := make(chan int, 1)
+			error_chn := make(chan error, 1)
 
-        // loop interno. Ogni ciclo è una mossa nella partita
-        for game==nil || game.state==Running {
-            move_chn := make(chan []byte, 1)
-            message_type_chn := make(chan int, 1)
-            error_chn := make(chan error, 1)
+			go func() {
+				messageType, move, err := conn.ReadMessage()
+				if err != nil {
+					error_chn <- err
+					return
+				}
+				message_type_chn <- messageType
+				move_chn <- move
+			}()
 
-            go func() {
-                messageType, move, err := conn.ReadMessage()
-                if err!=nil {
-                    error_chn <- err
-                    return
-                }
-                message_type_chn <- messageType
-                move_chn <- move
-            }()
+			var move []byte
+			var messageType int
+			var err error
 
-            var move []byte
-            var messageType int
-            var err error
+			if isFirstMove || !game_params.timed {
+				select {
+				case move = <-move_chn:
+					messageType = <-message_type_chn
+				case err = <-error_chn:
+				}
+			} else {
+				select {
+				case move = <-move_chn:
+					messageType = <-message_type_chn
+				case err = <-error_chn:
+				case <-timer:
+					changes := game.get_loosing_message()
+					game.state = Lost
+					send_changes(&changes, conn, game.state, false)
+					move = <-move_chn
+					messageType = <-message_type_chn
+				}
+			}
 
-            if isFirstMove || !game_params.timed {
-                select {
-                case move = <-move_chn:
-                    messageType = <-message_type_chn
-                case err = <-error_chn:
-                }
-            }else{
-                select {
-                case move = <-move_chn:
-                    messageType = <-message_type_chn
-                case err = <-error_chn:
-                case <-timer:
-                    changes := game.get_loosing_message()
-                    game.state=Lost
-                    send_changes(&changes, conn, game.state, false)
-                    move = <-move_chn
-                    messageType = <-message_type_chn
-                }
-            }
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			if messageType != 2 && messageType != 1 {
+				log.Println("BBB")
+				log.Println(messageType)
+				return
+			} // messageType: 1=text; 2=binary
+			if messageType == 1 {
+				s := string(move[:])
+				log.Println(s)
+				if s == "replay" {
+					break
+				} else {
+					return
+				}
+			}
 
-            if err != nil {
-                log.Println(err)
-                return
-            }
-            if messageType!=2 && messageType!=1 { 
-                log.Println("BBB");
-                log.Println(messageType)
-                return
-            } // messageType: 1=text; 2=binary
-            if messageType==1 {
-                s := string(move[:])
-                log.Println(s)
-                if (s=="replay") {
-                    break
-                }else{
-                    return
-                }
-            }
+			x, y, flag := bytesToMove(move)
 
-            x,y,flag := bytesToMove(move)
+			if isFirstMove {
+				if flag {
+					continue
+				}
+				game = NewGame(game_params.width, game_params.height,
+					game_params.bombs, game_params.tempo,
+					x, y)
+				startTime = time.Now()
+				timer = time.After(time.Duration(game_params.tempo) * time.Millisecond)
+				isFirstMove = false
+			}
 
-            if isFirstMove {
-                if flag { continue }
-                game = NewGame(game_params.width, game_params.height,
-                game_params.bombs, game_params.tempo,
-                x,y)
-                timer = time.After(time.Duration(game_params.tempo)*time.Millisecond)
-                isFirstMove=false
-            }
-
-            if flag {
-                flagged, err := game.flag(x, y)
-                if err==nil {
-                    send_flagged(flagged, x, y, conn,false)
-                }
-            }else{
-                timer = time.After(time.Duration(game_params.tempo)*time.Millisecond)
-                changes, err := game.click(x, y)
-                if err==nil {
-                    send_changes(&changes, conn, game.state, false)
-                }
-            }
-        }
-        game=nil
-        timer=nil
-    }
+			if flag {
+				flagged, err := game.flag(x, y)
+				if err == nil {
+					send_flagged(flagged, x, y, conn, false)
+				}
+			} else {
+				timer = time.After(time.Duration(game_params.tempo) * time.Millisecond)
+				changes, err := game.click(x, y)
+				if err == nil {
+					send_changes(&changes, conn, game.state, false)
+				}
+			}
+		}
+		game = nil
+		timer = nil
+		duration := time.Now().Sub(startTime)
+		log.Println("Game ended in ", duration.Milliseconds(), "ms")
+	}
 }
 
 func SinglePlayerPage(c *gin.Context) {
-    game_id := c.Query("game_id")
-    if game_id=="" {
-        c.Status(400)
-        return
-    }
-    if _, present := games[game_id]; !present {
-        c.Status(400)
-        return
-    }
-    shared.Render(c, http.StatusOK, "singlePlayer.html", gin.H{"game_id": game_id});
+	game_id := c.Query("game_id")
+	if game_id == "" {
+		c.Status(400)
+		return
+	}
+	if _, present := games[game_id]; !present {
+		c.Status(400)
+		return
+	}
+	shared.Render(c, http.StatusOK, "singlePlayer.html", gin.H{"game_id": game_id})
 }
 
-func get_timer(params *GameParams) <- chan time.Time{
-    if !params.timed {
-        return nil
-    }
-    return time.After(time.Duration(params.tempo) * time.Millisecond)
+func get_timer(params *GameParams) <-chan time.Time {
+	if !params.timed {
+		return nil
+	}
+	return time.After(time.Duration(params.tempo) * time.Millisecond)
 }
--- static/scripts/singlePlayer.js
+++ static/scripts/singlePlayer.js
@@ -7,13 +7,16 @@ const endgame_controls = document.querySelector("#endgame_controls");
 const play_again = document.querySelector("#replay");
 const goback = document.querySelector("#goback");
 
-socket.addEventListener("open", ()=>{
+socket.addEventListener("open", () => {
     socket.send(game_id);
 });
 
-let grid_width,grid_height,tot_bombs,time;
-let a; let b = new Promise(r=>{a=r});
-let cellSize=40;
+let grid_width, grid_height, tot_bombs, time;
+let a;
+let b = new Promise((r) => {
+    a = r;
+});
+let cellSize = 40;
 
 class Cella {
     constructor(flag, number, bomb) {
@@ -23,115 +26,163 @@ class Cella {
     }
 }
 /** @type{Cella[][]} */
-    const celle = [];
+const celle = [];
+
+let cronometro, total_time;
 
 async function setup() {
     await b;
-    let W = grid_width*cellSize;
-    let H = grid_height*cellSize;
+    let W = grid_width * cellSize;
+    let H = grid_height * cellSize;
     if (W > section.clientWidth) {
-        cellSize = section.clientWidth/grid_width;
-        W = grid_width*cellSize;
-        H = grid_height*cellSize;
+        cellSize = section.clientWidth / grid_width;
+        W = grid_width * cellSize;
+        H = grid_height * cellSize;
     }
-    if (H>window.innerHeight*0.9) {
-        cellSize = window.innerHeight*0.9/grid_height;
-        W = grid_width*cellSize;
-        H = grid_height*cellSize;
+    if (H > window.innerHeight * 0.9) {
+        cellSize = (window.innerHeight * 0.9) / grid_height;
+        W = grid_width * cellSize;
+        H = grid_height * cellSize;
     }
 
     const canvas = createCanvas(W, H);
     canvas.parent(box);
-    section.addEventListener('contextmenu', event => event.preventDefault());
+    section.addEventListener("contextmenu", (event) => event.preventDefault());
     resizeCollapsable();
 }
 
 let timer_start;
+const timer_span = document.getElementById("timer_span");
+const bomb_span = document.getElementById("bomb_span");
+function pad_left(n) {
+    while (str(n).length < 2) {
+        n = "0" + n;
+    }
+    return n;
+}
 function draw() {
-    if (phase==phases.GetGameParams) return;
+    if (phase == phases.GetGameParams) return;
+
+    const now = new Date();
+    if (cronometro && !total_time) {
+        const elapsed_time = now - cronometro;
+        timer_span.innerText =
+            pad_left(Math.floor(elapsed_time / 1000 / 60)) +
+            ":" +
+            pad_left(Math.floor(elapsed_time / 1000) % 60);
+    }
+
     background(200);
     stroke(150);
     strokeWeight(1);
-    for (let i=1; i<grid_width; i++) {
-        line(i*cellSize,0, i*cellSize,height);
+    for (let i = 1; i < grid_width; i++) {
+        line(i * cellSize, 0, i * cellSize, height);
     }
-    for (let i=1; i<grid_height; i++) {
-        line(0,i*cellSize, width, i*cellSize);
+    for (let i = 1; i < grid_height; i++) {
+        line(0, i * cellSize, width, i * cellSize);
     }
 
     textAlign(CENTER, CENTER);
-    textSize(cellSize*0.8);
+    textSize(cellSize * 0.8);
     noStroke();
-    for (let y=0; y<grid_height; y++) {
-        for (let x=0; x<grid_width; x++) {
+    for (let y = 0; y < grid_height; y++) {
+        for (let x = 0; x < grid_width; x++) {
             if (!celle[y][x]) continue;
             noStroke();
             if (celle[y][x].flag) {
                 fill(0);
-                text("🏳", (x+0.5)*cellSize, (y+0.5)*cellSize);
+                text("🏳", (x + 0.5) * cellSize, (y + 0.5) * cellSize);
                 continue;
             }
             fill(150);
-            rect(x*cellSize, y*cellSize, cellSize, cellSize);
+            rect(x * cellSize, y * cellSize, cellSize, cellSize);
             fill(0);
             if (celle[y][x].bomb) {
-                stroke(200,0,0);
-                line(x*cellSize, y*cellSize,(x+1)*cellSize, (y+1)*cellSize);
-                line((x+1)*cellSize, y*cellSize,x*cellSize, (y+1)*cellSize);
+                stroke(200, 0, 0);
+                line(
+                    x * cellSize,
+                    y * cellSize,
+                    (x + 1) * cellSize,
+                    (y + 1) * cellSize,
+                );
+                line(
+                    (x + 1) * cellSize,
+                    y * cellSize,
+                    x * cellSize,
+                    (y + 1) * cellSize,
+                );
                 noStroke();
                 continue;
             }
-            if (celle[y][x].number!=0) {
-                text(celle[y][x].number, (x+0.5)*cellSize, (y+0.5)*cellSize);
+            if (celle[y][x].number != 0) {
+                text(
+                    celle[y][x].number,
+                    (x + 0.5) * cellSize,
+                    (y + 0.5) * cellSize,
+                );
             }
         }
     }
 
     switch (phase) {
         case phases.GetUpdates:
-            if (time==0 || !timer_start) break;
-            const R = Math.min(width,height)*0.8
-            const c = color(180,40,40,30);
+            if (time == 0 || !timer_start) break;
+            const R = Math.min(width, height) * 0.8;
+            const c = color(180, 40, 40, 30);
             stroke(c);
             noFill();
             strokeWeight(20);
-            circle(width/2,height/2, R);
+            circle(width / 2, height / 2, R);
             noStroke();
             fill(c);
-            arc(width/2, height/2, R-20, R-20, map(new Date()-timer_start, 0, time, 0,TWO_PI)-PI/2, -PI/2);
+            arc(
+                width / 2,
+                height / 2,
+                R - 20,
+                R - 20,
+                map(now - timer_start, 0, time, 0, TWO_PI) - PI / 2,
+                -PI / 2,
+            );
             break;
         case phases.Won:
-            textSize(height/5);
+            textSize(height / 5);
             textStyle(BOLD);
-            fill(50,200,50);
-            stroke(50,100,50);
+            fill(50, 200, 50);
+            stroke(50, 100, 50);
             strokeWeight(2);
-            text("YOU WON", width/2, height/2);
+            text("YOU WON", width / 2, height / 2);
             textStyle(NORMAL);
             break;
         case phases.Lost:
-            textSize(height/5);
+            textSize(height / 5);
             textStyle(BOLD);
-            fill(200,0,0);
-            stroke(100,50,50);
+            fill(200, 0, 0);
+            stroke(100, 50, 50);
             strokeWeight(2);
-            text("YOU LOST", width/2, height/2);
+            text("YOU LOST", width / 2, height / 2);
             textStyle(NORMAL);
             break;
     }
 }
 
 function mousePressed() {
-    if (mouseX>width||mouseX<0||mouseY<0||mouseY>height) return true;
+    if (mouseX > width || mouseX < 0 || mouseY < 0 || mouseY > height)
+        return true;
     if (phase != phases.GetUpdates) return true;
-    const x = Math.floor(mouseX/cellSize);
-    const y = Math.floor(mouseY/cellSize);
-    const flag = mouseButton!=LEFT;
+
+    const x = Math.floor(mouseX / cellSize);
+    const y = Math.floor(mouseY / cellSize);
+    const flag = mouseButton != LEFT;
     const bits = new ArrayBuffer(5);
     const view = new DataView(bits);
     view.setInt16(0, x);
     view.setInt16(2, y);
-    view.setInt8(4, flag?1:0);
+    view.setInt8(4, flag ? 1 : 0);
+
+    if (!flag && !cronometro) {
+        cronometro = new Date();
+        total_time = null;
+    }
 
     socket.send(bits);
 
@@ -143,11 +194,11 @@ const phases = {
     GetUpdates: 2,
     Won: 3,
     Lost: 4,
-}
+};
 let phase = phases.GetGameParams;
 
-socket.addEventListener("message", e=>{
-    e.data.arrayBuffer().then(ab=>{
+socket.addEventListener("message", (e) => {
+    e.data.arrayBuffer().then((ab) => {
         const data_view = new DataView(ab);
 
         switch (phase) {
@@ -163,79 +214,85 @@ socket.addEventListener("message", e=>{
 
 function get_game_params(data_view) {
     const data = [];
-    for (let i=0; i<4; i++) {
-        data.push(data_view.getUint16(i*2))
+    for (let i = 0; i < 4; i++) {
+        data.push(data_view.getUint16(i * 2));
     }
-    [grid_width,grid_height,tot_bombs,time] = data;
+    [grid_width, grid_height, tot_bombs, time] = data;
     a();
     phase = phases.GetUpdates;
-    for (let y=0; y<grid_width; y++) {
-        celle.push([])
-        for (let x=0; x<grid_width; x++) {
-            celle[y].push(null)
+    for (let y = 0; y < grid_width; y++) {
+        celle.push([]);
+        for (let x = 0; x < grid_width; x++) {
+            celle[y].push(null);
         }
     }
 }
 
 /**
-    * @param {DataView} data_view 
-    */
+ * @param {DataView} data_view
+ */
 
-    function get_updates(data_view) {
-        const first_byte = data_view.getInt8(0);
-        type = first_byte >> 6;
-        switch (type) {
-            case 0:
-                const gameover = (first_byte & 0b00100000)>0;
-                const won = (first_byte & 0b00010000)>0;
-                const lost = gameover && !won;
-                let off = 1;
-                let has_next = true;
-                do {
-                    const x = data_view.getUint16(off); off+=2;
-                    const y = data_view.getUint16(off); off+=2;
-                    const payload = data_view.getUint8(off); off+=1;
-                    const num = payload>>4;
-                    has_next = (payload&0b00001000)>0;
+function get_updates(data_view) {
+    const first_byte = data_view.getInt8(0);
+    type = first_byte >> 6;
+    switch (type) {
+        case 0:
+            const gameover = (first_byte & 0b00100000) > 0;
+            const won = (first_byte & 0b00010000) > 0;
+            const lost = gameover && !won;
+            let off = 1;
+            let has_next = true;
+            do {
+                const x = data_view.getUint16(off);
+                off += 2;
+                const y = data_view.getUint16(off);
+                off += 2;
+                const payload = data_view.getUint8(off);
+                off += 1;
+                const num = payload >> 4;
+                has_next = (payload & 0b00001000) > 0;
 
-                    if (lost) {
-                        celle[y][x] = new Cella(false, 0, true);
-                    }else{
-                        celle[y][x] = new Cella(false, num, false);
-                    }
-                } while (has_next);
-
-                if (gameover) {
-                    phase = won ? phases.Won : phases.Lost;
-                    console.log("timer null");
-                    timer_start=null
-                    show_endgame_controls();
-                    return;
+                if (lost) {
+                    celle[y][x] = new Cella(false, 0, true);
                 } else {
-                    timer_start = new Date();
+                    celle[y][x] = new Cella(false, num, false);
                 }
-                break;
-            case 1:
-                const flag = (first_byte&0b00100000)>0;
-                const x = data_view.getUint16(1);
-                const y = data_view.getUint16(3);
-                if (flag) {
-                    celle[y][x] = new Cella(true, 0, false);
-                }else{
-                    celle[y][x] = null;
-                }
-                break;
-        }
+            } while (has_next);
+
+            if (gameover) {
+                total_time = new Date() - cronometro;
+                cronometro = null;
+
+                phase = won ? phases.Won : phases.Lost;
+                console.log("timer null");
+                timer_start = null;
+                show_endgame_controls();
+                return;
+            } else {
+                timer_start = new Date();
+            }
+            break;
+        case 1:
+            const flag = (first_byte & 0b00100000) > 0;
+            const x = data_view.getUint16(1);
+            const y = data_view.getUint16(3);
+            if (flag) {
+                celle[y][x] = new Cella(true, 0, false);
+            } else {
+                celle[y][x] = null;
+            }
+            break;
     }
+}
 
 function show_endgame_controls() {
     endgame_controls.classList.add("form_fields");
     resizeCollapsable();
 }
 
-play_again.addEventListener("click", ()=>{
+play_again.addEventListener("click", () => {
     for (let row of celle) {
-        for (let i=0; i<row.length; i++) {
+        for (let i = 0; i < row.length; i++) {
             row[i] = null;
         }
     }
