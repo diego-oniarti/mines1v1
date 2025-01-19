8da18b2e6de983298ca365d3f65e343339766518
Diego <69463084+diego-oniarti@users.noreply.github.com>
2025-01-19 21:50:41 +0100

    test2

--- redirect.js
+++ redirect.js
@@ -1,1 +1,4 @@
-fetch("https://horse-smooth-mutt.ngrok-free.app/minesredirect").then(res=>res.json()).then(data=>{console.log(data)})
+fetch("https://horse-smooth-mutt.ngrok-free.app/minesredirect").then(res=>res.json()).then(data=>{
+    console.log(data);
+    window.location.replace(data.url);
+})
